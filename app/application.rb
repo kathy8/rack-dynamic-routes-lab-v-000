8914ef19a31f1c1a012d1d28d6a6d7d57b4a2e0c
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-16 20:35:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
 
-  @@items = [item.new("Figs", 3.42,Item.new("Pears", 0.99)]
+  @@items = [item.new("Figs",3.42,Item.new("Pears",0.99)]
 
   def call(env)
     resp = Rack::Response.new
