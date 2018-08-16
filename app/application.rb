bb93704fac85aa4ed02f30d38ec677c7f07cafb8
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-08-16 20:29:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,11 @@
 class Application
 
   @@items = [item.new("plums", 4.00)]
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path.match(/items/)
        item_name = req.path.split("items/").last
        item = @@items.find{|i| i.name == item_name}
@@ -19,7 +19,7 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
